5274f11b36e9f516820a5c0ec027a54b3e1b1821
hsj1104 <39845210+hsj1104@users.noreply.github.com>
2024-05-23 18:16:47 +0800

    Update createAiObj.tsx

--- src/helpers/createAiObj.tsx
+++ src/helpers/createAiObj.tsx
@@ -1,5 +1,5 @@
 import { GoogleGenerativeAI as OfficialGoogleGenerativeAI } from "@google/generative-ai";
-import { GoogleGenerativeAI as thirdPartyGoogleGenerativeAI } from "@fuyun/generative-ai";
+import { GoogleGenerativeAI as thirdPartyGoogleGenerativeAI } from "@redchicken2024/generative-ai";
 
 export const createAiObj = (key: string, api: string | null) => {
     if (api !== null) {
